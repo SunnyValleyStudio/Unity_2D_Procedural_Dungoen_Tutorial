94f7d12b3006ee6410828c4a3f69ae9b9cc5d5b2
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-12-16 11:05:19 +0100

    Video 5 - Random Walk Generator class

---
+++ _Scripts/SimpleRandomWalkDungeonGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SimpleRandomWalkDungeonGenerator : MonoBehaviour
+{
+    [SerializeField]
+    protected Vector2Int startPosition = Vector2Int.zero;
+
+    [SerializeField]
+    private int iterations = 10;
+    [SerializeField]
+    public int walkLength = 10;
+    [SerializeField]
+    public bool startRandomlyEachIteration = true;
+
+    public void RunProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        foreach (var position in floorPositions)
+        {
+            Debug.Log(position);
+        }
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk()
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+        for (int i = 0; i < iterations; i++)
+        {
+            var path = ProceduralGenerationAlgorithms.SimpleRandomWalk(currentPosition, walkLength);
+            floorPositions.UnionWith(path);
+            if (startRandomlyEachIteration)
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+        }
+        return floorPositions;
+    }
+}
