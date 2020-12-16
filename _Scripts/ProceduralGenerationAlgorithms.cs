efeae94d2ed78dbc54532a9c91d8b044848a4a5f
SunnyValleyStudio <piotrmgrupa4@gmail.com>
2020-12-16 10:38:16 +0100

    Video 4 - Random Walk Algorithm

---
+++ _Scripts/ProceduralGenerationAlgorithms.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProceduralGenerationAlgorithms
+{
+    
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+        var previousPosition = startPosition;
+
+        for (int i = 0; i < walkLength; i++)
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
+            path.Add(newPosition);
+            previousPosition = newPosition;
+        }
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionsList = new List<Vector2Int>
+    {
+        new Vector2Int(0,1), //UP
+        new Vector2Int(1,0), //RIGHT
+        new Vector2Int(0, -1), // DOWN
+        new Vector2Int(-1, 0) //LEFT
+    };
+
+    public static Vector2Int GetRandomCardinalDirection()
+    {
+        return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+    }
+}
